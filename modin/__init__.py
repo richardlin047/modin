51e2fdc107763d2cc1662a33c9e5d2620f76a6e8
Richard Lin <richard.lin.047@berkeley.edu>
2020-10-21 15:19:22 -0400

    Comment and remove irrelevant code from original implementation

--- modin/__init__.py
+++ modin/__init__.py
@@ -58,64 +58,66 @@ def set_backends(engine=None, partition=None):
 
 
 ### MERGE CONFLICT BEGIN
-def get_execution_engine():
-    # In the future, when there are multiple engines and different ways of
-    # backing the DataFrame, there will have to be some changed logic here to
-    # decide these things. In the meantime, we will use the currently supported
-    # execution engine + backing (Pandas + Ray).
-    if "MODIN_ENGINE" in os.environ:
-        if os.environ["MODIN_ENGINE"].title() == "Mpi":
-            return "MPI"
-        # .title allows variants like ray, RAY, Ray
-        return os.environ["MODIN_ENGINE"].title()
-    else:
-        if "MODIN_DEBUG" in os.environ:
-            return "Python"
-        else:
-            if sys.platform != "win32":
-                try:
-                    import ray
+# import os, sys
+# def get_execution_engine():
+#     # In the future, when there are multiple engines and different ways of
+#     # backing the DataFrame, there will have to be some changed logic here to
+#     # decide these things. In the meantime, we will use the currently supported
+#     # execution engine + backing (Pandas + Ray).
+#     if "MODIN_ENGINE" in os.environ:
+#         if os.environ["MODIN_ENGINE"].title() == "Mpi":
+#             return "MPI"
+#         # .title allows variants like ray, RAY, Ray
+#         return os.environ["MODIN_ENGINE"].title()
+#     else:
+#         if "MODIN_DEBUG" in os.environ:
+#             return "Python"
+#         else:
+#             if sys.platform != "win32":
+#                 try:
+#                     import ray
 
-                except ImportError:
-                    pass
-                else:
-                    if version.parse(ray.__version__) != version.parse("0.8.0"):
-                        raise ImportError(
-                            "Please `pip install modin[ray]` to install compatible Ray version."
-                        )
-                    return "Ray"
-            try:
-                import dask
-                import distributed
+#                 except ImportError:
+#                     pass
+#                 else:
+#                     if version.parse(ray.__version__) != version.parse("0.8.0"):
+#                         raise ImportError(
+#                             "Please `pip install modin[ray]` to install compatible Ray version."
+#                         )
+#                     return "Ray"
+#             try:
+#                 import dask
+#                 import distributed
 
-            except ImportError:
-                raise ImportError(
-                    "Please `pip install {}modin[dask]` to install an engine".format(
-                        "modin[ray]` or `" if sys.platform != "win32" else ""
-                    )
-                )
-            else:
-                if version.parse(dask.__version__) < version.parse(
-                    "2.1.0"
-                ) or version.parse(distributed.__version__) < version.parse("2.3.2"):
-                    raise ImportError(
-                        "Please `pip install modin[dask]` to install compatible Dask version."
-                    )
-                return "Dask"
-
-
-def get_partition_format():
-    # See note above about engine + backing.
-    return os.environ.get("MODIN_BACKEND", "Pandas").title()
+#             except ImportError:
+#                 raise ImportError(
+#                     "Please `pip install {}modin[dask]` to install an engine".format(
+#                         "modin[ray]` or `" if sys.platform != "win32" else ""
+#                     )
+#                 )
+#             else:
+#                 if version.parse(dask.__version__) < version.parse(
+#                     "2.1.0"
+#                 ) or version.parse(distributed.__version__) < version.parse("2.3.2"):
+#                     raise ImportError(
+#                         "Please `pip install modin[dask]` to install compatible Dask version."
+#                     )
+#                 return "Dask"
 
 
-__version__ = "0.6.3"
-__execution_engine__ = get_execution_engine()
-__partition_format__ = get_partition_format()
+# def get_partition_format():
+#     # See note above about engine + backing.
+#     return os.environ.get("MODIN_BACKEND", "Pandas").title()
 
-# We don't want these used outside of this file.
-del get_execution_engine
-del get_partition_format
+
+# __version__ = "0.6.3"
+# __execution_engine__ = get_execution_engine()
+# __partition_format__ = get_partition_format()
+
+# # We don't want these used outside of this file.
+# del get_execution_engine
+# del get_partition_format
 ### MERGE CONFLICT END
+
 __version__ = get_versions()["version"]
 del get_versions
