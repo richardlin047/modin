12a022a74c92305bb472add70bab39cd5a0a4fe1
Richard Lin <richard.lin.047@berkeley.edu>
2021-05-12 13:58:17 -0400

    Update script

--- richard_startup.sh
+++ richard_startup.sh
@@ -8,7 +8,7 @@ TYPE=$5
 
 echo "Installing wheel..."
 sudo -u "$USERNAME" -i /bin/bash -l -c "conda init bash"
-sudo -u "$USERNAME" -i /bin/bash -l -c "conda activate $CONDA_ENV; pip install $WHEEL; git clone https://github.com/richardlin047/modin.git; cd modin; git pull origin partition_benchmark; python scaleup_dataframe.py""
+sudo -u "$USERNAME" -i /bin/bash -l -c "conda activate $CONDA_ENV; pip install $WHEEL; git clone https://github.com/richardlin047/modin.git; cd modin; git pull origin partition_benchmark; python scaleup_dataframe.py"
 
 echo "Setting up service scripts..."
 cat > /home/"$USERNAME"/ray-head.sh << EOM
